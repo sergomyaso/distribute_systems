fb070d2f650c5d0ba2556f4b580207a8a74e41fe
Мясоедов Сергей <masoedovsergej@MacBook-Pro-Administrator.local>
2023-04-28 01:45:44 +0700

    add manager rabbit support

--- manager/src/main/kotlin/ru/nsu/manager/ManagerApplication.kt
+++ manager/src/main/kotlin/ru/nsu/manager/ManagerApplication.kt
@@ -1,5 +1,6 @@
 package ru.nsu.manager
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
