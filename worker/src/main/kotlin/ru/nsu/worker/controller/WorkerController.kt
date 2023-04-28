512fdd7a287f12bdf3474f09f5bebec0920f27ef
Мясоедов Сергей <masoedovsergej@MacBook-Pro-Administrator.local>
2023-04-28 13:53:24 +0700

    Fixe makefile

--- worker/src/main/kotlin/ru/nsu/worker/controller/WorkerController.kt
+++ worker/src/main/kotlin/ru/nsu/worker/controller/WorkerController.kt
@@ -1,6 +1,9 @@
 package ru.nsu.worker.controller
 
+import com.rabbitmq.client.CancelCallback
 import com.rabbitmq.client.Connection
+import com.rabbitmq.client.DeliverCallback
+import com.rabbitmq.client.Delivery
 import jakarta.xml.bind.JAXBContext
 import jakarta.xml.bind.JAXBException
 import jakarta.xml.bind.Marshaller
@@ -22,11 +25,13 @@ import ru.nsu.ccfit.schema.crack_hash_response.CrackHashManagerRequest
 import ru.nsu.ccfit.schema.crack_hash_response.CrackHashWorkerResponse
 import ru.nsu.ccfit.schema.crack_hash_response.CrackHashWorkerResponse.Answers
 import ru.nsu.worker.configrutaions.RabbitQueue
+import java.io.ByteArrayInputStream
 import java.io.IOException
 import java.io.StringWriter
 import java.math.BigInteger
 import java.security.MessageDigest
 import java.time.Duration
+import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.Executors
 import kotlin.math.ceil
 import kotlin.math.pow
@@ -37,7 +42,31 @@ import kotlin.math.pow
 class WorkerController @Autowired constructor(
     private val rabbitConn: Connection,
     private val workerQueue: RabbitQueue,
-    ) {
+) {
+    private val queueManagerResp: ConcurrentLinkedQueue<CrackHashManagerRequest> = ConcurrentLinkedQueue()
+    val WORKER_EXCHANGE = "worker_exchange"
+    val MANAGE_EXCHANGE = "manager_exchange"
+
+    init {
+        rabbitConn.createChannel().use {
+            val deliverCallback = DeliverCallback { consumerTag: String?, delivery: Delivery? ->
+                val message = delivery?.body?.let { String(it, Charsets.UTF_8) }
+                println("Received '$message'")
+
+                val jaxbContext = JAXBContext.newInstance(CrackHashManagerRequest::class.java)
+                val unmarshaller = jaxbContext.createUnmarshaller()
+                val resp: CrackHashManagerRequest;
+                if (message != null) {
+                    resp =
+                        unmarshaller.unmarshal(ByteArrayInputStream(message.toByteArray())) as CrackHashManagerRequest
+                    queueManagerResp.add(resp);
+                }
+                it.basicAck(delivery?.envelope?.deliveryTag ?: 0, false)
+            }
+
+            it.basicConsume(workerQueue.queueName, false, deliverCallback, CancelCallback { })
+        }
+    }
 
     private val restTemplate = RestTemplateBuilder()
         .rootUri("http://manager:8080")
@@ -88,11 +117,21 @@ class WorkerController @Autowired constructor(
         taskResponse.requestId = requestId
         taskResponse.answers = convertToAnswers(matchedWords)
         taskResponse.partNumber = partNumber
-        val entity = HttpEntity(taskResponse)
-        val response = restTemplate.exchange(
-            "/internal/api/manager/hash/crack/request", HttpMethod.PATCH, entity,
-            String::class.java
-        )
+        sendResponseToManagerQueue(taskResponse)
+
+        sendResponseToManagerQueue(taskResponse)
+//        val entity = HttpEntity(taskResponse)
+//        val response = restTemplate.exchange(
+//            "/internal/api/manager/hash/crack/request", HttpMethod.PATCH, entity,
+//            String::class.java
+//        )
+    }
+
+    private fun sendResponseToManagerQueue(req: CrackHashWorkerResponse) {
+//        val jaxbContext = JAXBContext.newInstance(CrackHashWorkerResponse::class.java)
+//        val marshaller = jaxbContext.createMarshaller()
+//
+        rabbitConn.createChannel().basicPublish(WORKER_EXCHANGE, "", null, req.toString().toByteArray())
     }
 
     private fun convertToAnswers(words: List<String>): Answers {
@@ -119,8 +158,8 @@ class WorkerController @Autowired constructor(
 
     @Scheduled(fixedRate = 30)
     fun checkQueueWorkerResponses() {
-        if (.size > 0) {
-            submitHashes(queueWorkerResp.poll())
+        if (queueManagerResp.size > 0) {
+            executorService.submit { doWork(queueManagerResp.poll()) }
         }
     }
 
